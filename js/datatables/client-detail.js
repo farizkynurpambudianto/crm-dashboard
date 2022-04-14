6f0566822920247c6b8f15f11021126ca311c5ab
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2022-04-14 14:21:36 +0700

    adjusting client-detail hifi

---
+++ js/datatables/client-detail.js
@@ -0,0 +1,12 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function () {
+  $('#projectTable', '#prospectTable').DataTable({
+    select: true,
+    dom: '<"top">rt<"bottom-table">',
+    lengthMenu: [
+      [5, 10, 25, 50, -1],
+      [5, 10, 25, 50, 'All']
+    ],
+    ordering: false
+  })
+})
